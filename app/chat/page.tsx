7a369a5fb18cc2347ab74a6f5910ff2715017cb0
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2025-09-24 23:19:35 +0100

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -750,13 +750,15 @@ export default function SandboxPage() {
           {analysisStatus && <p className="text-sm text-[rgba(55,50,47,0.60)] mb-3 animate-pulse">{analysisStatus}</p>}
           <div className="relative flex flex-col h-[300px]">
             <div className=" flex flex-col  rounded-t-[10px] bg-transparent border-b border-[rgba(55,50,47,0.12)] h-[60px] w-full">
-              <div className="w-full p-1 rounded-t-[10px]  h-[50%] p-[2px] flex items-center border border-b-none border-[rgba(55,50,47,0.12)] gap-1">
+             <div className="w-full h-[30px] flex items-center justify-center">
+                <div className="w-[70%] p-1 rounded-t-[10px]  h-[50%] p-[2px] flex items-center border border-[rgba(55,50,47,0.12)] gap-1">
                 <div className="w-auto p-1 h-[25px] border border-black rounded-[12px] flex items-center justify-center">
-                  <AtSign size={18} />
+                  <AtSign size={16} />
                   <p>Mention</p>
                 </div>
               </div>
-              <div className="w-full p-1 h-[50%] border-t border-[rgba(55,50,47,0.12)] p-[2px] flex items-center gap-1">
+             </div>
+              <div className="w-full p-1 h-[50%] border border-[rgba(55,50,47,0.12)] p-[2px] flex items-center gap-1">
                 <div className="w-[25px] p-1 h-[25px] border border-black rounded-[12px] flex items-center justify-center">
                   <Plus size={18} />
                 </div>
