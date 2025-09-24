aa05e44ea1c8c4e048e37c683789c9db5e8ed59a
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2025-09-24 23:32:25 +0100

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -751,26 +751,26 @@ export default function SandboxPage() {
           <div className="relative flex flex-col h-[300px]">
             <div className=" flex flex-col  rounded-t-[10px] bg-transparent border-b border-[rgba(55,50,47,0.12)] h-[60px] w-full">
              <div className="w-full h-[50%] flex items-center justify-center">
-                <div className="w-[90%] p-1 rounded-t-[8px]  h-full p-[2px] flex items-center border border-[rgba(55,50,47,0.12)] gap-1">
-                <div className="w-auto p-1 h-[25px] border border-black rounded-[12px] flex items-center justify-center">
+                <div className="w-[95%] p-1 rounded-t-[8px]  h-full p-[2px] flex items-center border border-[rgba(55,50,47,0.12)] gap-1">
+                <div className="w-auto p-1 h-[20px] border border-black rounded-[12px] flex items-center justify-center">
                   <AtSign size={16} />
-                  <p>Mention</p>
+                  <p className="text-sm">Mention</p>
                 </div>
               </div>
              </div>
-              <div className="w-full p-1 h-[50%] border border-[rgba(55,50,47,0.12)] p-[2px] flex items-center gap-1">
+              <div className="w-full p-1 h-[50%] border border-b-none border-[rgba(55,50,47,0.12)] p-[2px] flex items-center gap-1">
                 <div className="w-[25px] p-1 h-[25px] border border-black rounded-[12px] flex items-center justify-center">
-                  <Plus size={18} />
+                  <Plus size={16} />
                 </div>
-                <div className="w-auto p-1 h-[25px] border border-black rounded-[12px] flex items-center justify-center">
-                  <AtSign size={18} />
+                <div className="w-auto p-1 h-[20px] border border-black rounded-[12px] flex items-center justify-center">
+                  <AtSign size={16} />
                   <p>Mention</p>
                 </div>
               </div>
             </div>
             <textarea
               placeholder={currentProject ? "Describe what to build..." : "Please create or select a project first."}
-              className="w-full border border-[rgba(55,50,47,0.12)] p-4 pr-28  resize-none text-sm bg-[#F7F5F3] text-[#37322F] placeholder:text-[rgba(55,50,47,0.60)] focus:outline-none focus:ring-2 focus:ring-[rgba(55,50,47,0.12)] focus:border-transparent"
+              className="w-full border border-t-none border-[rgba(55,50,47,0.12)] p-4 pr-28  resize-none text-sm bg-[#F7F5F3] text-[#37322F] placeholder:text-[rgba(55,50,47,0.60)] focus:outline-none focus:ring-2 focus:ring-[rgba(55,50,47,0.12)] focus:border-transparent"
               rows={3}
               value={chatInput}
               onChange={(e) => setChatInput(e.target.value)}
