38dbdc60bf4b3350b37973f4e568bdf9867ff4d5
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2025-09-24 22:39:58 +0100

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -749,8 +749,17 @@ export default function SandboxPage() {
         <div className="p-6  border-[rgba(55,50,47,0.12)]  flex-shrink-0">
           {analysisStatus && <p className="text-sm text-[rgba(55,50,47,0.60)] mb-3 animate-pulse">{analysisStatus}</p>}
           <div className="relative">
-            <div className="absolute flex items-center justify-center -top-3 rounded-t-[10px] bg-white h-[30px] w-full">
-              bonjour
+            <div className="absolute flex flex-col -top-3 rounded-t-[10px] bg-transparent border-b border-[rgba(55,50,47,0.12)] h-[60px] w-full">
+              <div className="w-full h-[50%] p-[2px] flex items-center gap-1">bonjour</div>
+              <div className="w-full h-[50%] border-t border-[rgba(55,50,47,0.12)] p-[2px] flex items-center gap-1">
+                <div className="w-[25px] p-1 h-[25px] border border-black rounded-[12px] flex items-center justify-center">
+                  <Plus size={18} />
+                </div>
+                <div className="w-auto p-1 h-[25px] border border-black rounded-[12px] flex items-center justify-center">
+                  <AtSign size={18} />
+                  <p>Mention</p>
+                </div>
+              </div>
             </div>
             <textarea
               placeholder={currentProject ? "Describe what to build..." : "Please create or select a project first."}
