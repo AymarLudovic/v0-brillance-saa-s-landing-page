9fc8eadaf77200c9189f202bb76fbfe745ecb681
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2025-09-24 23:04:03 +0100

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -748,8 +748,8 @@ export default function SandboxPage() {
 
         <div className="p-1 h-[300px] border-[rgba(55,50,47,0.12)]  flex-shrink-0">
           {analysisStatus && <p className="text-sm text-[rgba(55,50,47,0.60)] mb-3 animate-pulse">{analysisStatus}</p>}
-          <div className="relative h-[300px]">
-            <div className="absolute flex flex-col -top-5 rounded-t-[10px] bg-transparent border-b border-[rgba(55,50,47,0.12)] h-[60px] w-full">
+          <div className="relative flex flex-col h-[300px]">
+            <div className=" flex flex-col  rounded-t-[10px] bg-transparent border-b border-[rgba(55,50,47,0.12)] h-[60px] w-full">
               <div className="w-full p-1 h-[50%] p-[2px] flex items-center gap-1">bonjour</div>
               <div className="w-full p-1 h-[50%] border-t border-[rgba(55,50,47,0.12)] p-[2px] flex items-center gap-1">
                 <div className="w-[25px] p-1 h-[25px] border border-black rounded-[12px] flex items-center justify-center">
