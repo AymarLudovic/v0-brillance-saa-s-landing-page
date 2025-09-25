05e9f66ee81556f98328cbdd66f13f79b9f372c4
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2025-09-25 03:37:50 +0100

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -755,7 +755,7 @@ export default function SandboxPage() {
         
           
       <div className="w-[60%] h-full flex flex-col bg-white">
-        <div className="flex items-center justify-between p-4 flex-shrink-0 h-16 border-b border-[rgba(55,50,47,0.12)]">
+        <div className="flex items-center justify-between p-4 flex-shrink-0 h-12 border-b border-[rgba(55,50,47,0.12)]">
           <div className="bg-[#F7F5F3] rounded-xl h-10 flex items-center p-1 border border-[rgba(55,50,47,0.12)]">
             <Button
               variant={activeTab === "preview" ? "secondary" : "ghost"}
@@ -783,7 +783,10 @@ export default function SandboxPage() {
                 className="flex items-center justify-center rounded-xl border border-[rgba(55,50,47,0.12)] bg-white p-2 hover:bg-[#F7F5F3] transition-colors h-10 w-10"
                 aria-label="GitHub"
               >
-                <Github className="h-5 w-5 text-[#37322F]" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#37322F" className="h-[18px] w-[18px]" viewBox="0 0 16 16">
+  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+</svg>
+                
               </button>
               <button className="rounded-full text-white flex items-center justify-center transition hover:brightness-90 h-10 px-6 bg-[#37322F] hover:bg-[rgba(55,50,47,0.90)]">
                 Deploy
@@ -836,7 +839,7 @@ export default function SandboxPage() {
           
           {activeTab === "preview" ? (
             <div className="flex-grow flex flex-col overflow-hidden w-full h-full">
-              <div className="flex-grow bg-white w-full border border-[rgba(55,50,47,0.12)] m-4  overflow-hidden">
+              <div className="flex-grow bg-white w-full border border-[rgba(55,50,47,0.12)] m-1 h-full  overflow-hidden">
                 {previewUrl ? (
                   <iframe ref={iframeRef} src={previewUrl} className="w-full h-full border-0" title="Sandbox Preview" />
                 ) : (
