0e9677b30feaa080ecfce201bed97f202a42f6fb
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2025-12-05 00:28:43 +0100

    Update route.ts

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -62,6 +62,79 @@ Si l'utilisateur demande : "Crée un bouton pour supprimer un utilisateur".
   4. Un Toast de confirmation au retour de l'action.
 
 En résumé : Ton code doit être prêt à être déployé et utilisé, pas juste regardé.
+
+
+INTÉGRATION DE MODEL D'IA AU CAS OÙ L'UTILISATEUR TE DEMANDE DE CRÉÉ QUELQUES CHOSES EN RAPPORT AVEC EUX :
+==== NANO BANANA (IMAGES GÉNÉRATION ET MODIFICATION - LOW TIER)===
+CODE:
+ // To run this code you need to install the following dependencies:
+// npm install @google/genai mime
+// npm install -D @types/node
+Note que les symboles en dollars que tu vois de cette manière [$] quand tu intégrera le code dans le projet de l'utilisateur, tu enleveras l'encadrement [] sur le symbole du dollar la.
+Demande toujours à l'utilisateur de t'envoyer sa clé api de Gemini pour cette intégration, car tu l'ajoutera dans un fichier .env que tu créera. 
+import {
+  GoogleGenAI,
+} from '@google/genai';
+import mime from 'mime';
+import { writeFile } from 'fs';
+
+function saveBinaryFile(fileName: string, content: Buffer) {
+  writeFile(fileName, content, 'utf8', (err) => {
+    if (err) {
+      console.error(\`Error writing file [$]{fileName}:\`, err);
+      return;
+    }
+    console.log(\`File [$]{fileName} saved to file system.\`);
+  });
+}
+
+async function main() {
+  const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+  });
+  const config = {
+    responseModalities: [
+        'IMAGE',
+        'TEXT',
+    ],
+  };
+  const model = 'gemini-2.5-flash-image';
+  const contents = [
+    {
+      role: 'user',
+      parts: [
+        {
+          text: \`INSERT_INPUT_HERE\`,
+        },
+      ],
+    },
+  ];
+
+  const response = await ai.models.generateContentStream({
+    model,
+    config,
+    contents,
+  });
+  let fileIndex = 0;
+  for await (const chunk of response) {
+    if (!chunk.candidates || !chunk.candidates[0].content || !chunk.candidates[0].content.parts) {
+      continue;
+    }
+    if (chunk.candidates?.[0]?.content?.parts?.[0]?.inlineData) {
+      const fileName = \`ENTER_FILE_NAME_[$]{fileIndex++}\`;
+      const inlineData = chunk.candidates[0].content.parts[0].inlineData;
+      const fileExtension = mime.getExtension(inlineData.mimeType || '');
+      const buffer = Buffer.from(inlineData.data || '', 'base64');
+      saveBinaryFile(\`[$]{fileName}\[.$]{fileExtension}\`, buffer);
+    }
+    else {
+      console.log(chunk.text);
+    }
+  }
+}
+
+main();
+
 \n\n
 ${basePrompt}\n\n
 `; 
