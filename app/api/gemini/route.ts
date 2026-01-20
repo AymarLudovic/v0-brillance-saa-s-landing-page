55779eba0d44961a1e1104c73f4df7fe170c50e2
AymarLudovic <134813856+AymarLudovic@users.noreply.github.com>
2026-01-20 19:18:07 +0100

    Update route.ts

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -92,15 +92,8 @@ export async function POST(req: Request) {
                 config: { 
                     // Injection massive du prompt unique
                     systemInstruction: FULL_PROMPT_INJECTION
-                },
-                generationConfig: {
-  temperature: 1.5,             // Low value to force precision on critical instructions
-  maxOutputTokens: 9192,
-  thinkingConfig: {     // New feature of Gemini 3 (2026)
-    includeThoughts: true,
-    thinkingLevel: "high" // Forces internal reasoning before responding
-    }
                 }
+                
                 
             });
 
